4ca23284ca29cd8e42cb47596453c28572e013ec
Tim Koschützki <tim@debuggable.com>
2011-01-21 16:53:19 +0100

    changing algorithmn for 32bit uuids

--- cake/libs/string.php
+++ cake/libs/string.php
@@ -53,12 +53,25 @@ class String extends Object {
  * @static
  */
 	function uuid() {
-		$uuid = '';
-		for ($i = 0; $i < 32; $i++) {
-			$rand = lcg_value() * 0.999999999999; // make sure $rand can never be 1
-			$num = dechex(floor($rand * 16));
-			$uuid .= $num;
-		}
+	    $uuid = sprintf('%04x%04x%04x%04x%04x%04x%04x%04x',
+			// 32 bits for "time_low"
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_mid"
+			mt_rand( 0, 0xffff ),
+
+			// 16 bits for "time_hi_and_version",
+			// four most significant bits holds version number 4
+			mt_rand( 0, 0x0fff ) | 0x4000,
+
+			// 16 bits, 8 bits for "clk_seq_hi_res",
+			// 8 bits for "clk_seq_low",
+			// two most significant bits holds zero and one for variant DCE1.1
+			mt_rand( 0, 0x3fff ) | 0x8000,
+
+			// 48 bits for "node"
+			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+	    );
 		return $uuid;
 	}
 /**
